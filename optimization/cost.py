6499fb99b73c13efacd104d20a4c8acd3615706d
omariyassine <yassine.omari-betahi@qantev.com>
2020-06-29 03:28:19 +0200

    force verbose in optimization

--- optimization/cost.py
+++ optimization/cost.py
@@ -1,6 +1,10 @@
 import numpy as np
+import logging
+
+logger = logging.getLogger(__file__)
 
 
 def cost_function(extra_energy, potential_increase=None, pred=None):
     cost = np.sum(-extra_energy * (potential_increase ** 2) / 1000)
+    logger.debug("Cost function: %s", cost)
     return cost
